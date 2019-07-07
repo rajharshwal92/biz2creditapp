13f5d0d24c6e6948016fced52d874084cbbe7f2f
Kuldeep Harshwal <kuldeep.harshwal@allonblock.com>
2019-07-07 19:48:38 +0530

    Added customer list and invite customer logic

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Biz2creditApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.0.
+
+## How to Run this Application
+
+Need following dependencies
+
+NodeJs >= 10.16
+Angular CLI Latest (version 8.1.0) (It is built on latest angular CLI).
+
+## Step to do
+
+Goto project directory
+
+Run `npm install` For install node modules or dependency modules (which is angular/material, lodash etc.).
+
+Run `ng serve --open` for start dev server and open browser tab on `http://localhost:4200/`.
+
+After that there are a input box where User can upload `customer.txt`.
+
+After successfully uploaded `customer.txt` User can see customer list on screen.
+
+There are invite button for invite customer within 100Km.
 
 ## Development server
 
@@ -25,3 +45,4 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material';
+import { sortBy as _sortBy } from 'lodash';
+
+import distance from './shared/utility';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('table', null) table: MatTable<Element>;
   title = 'biz2creditApp';
+  fileContent: any = '';
+  showInvite: boolean = false;
+  displayedColumns: string[] = ['user_id', 'name', 'latitude', 'longitude', 'dist'];
+  dataSource: any = [];
+  dublinGPSCord = {
+    latitude: 53.339428,
+    longitude: -6.257664
+  };
+
+  onChange(fileList: FileList): void {
+    let file = fileList[0];
+    let fileReader: FileReader = new FileReader();
+    let self = this;
+    const dablinLat = this.dublinGPSCord.latitude;
+    const dablinLong = this.dublinGPSCord.longitude;
+    fileReader.onloadend = function (x) {
+      self.fileContent = fileReader.result;
+      let fileData = self.fileContent.split('\r\n');
+      for (let item of fileData) {
+        const customer = JSON.parse(item);
+        customer.dist = distance(dablinLat, dablinLong, customer.latitude, customer.longitude, 'K');
+        self.dataSource.push(customer);
+      }
+      self.dataSource = _sortBy(self.dataSource, 'user_id');
+      self.table.renderRows();
+      self.showInvite = true;
+    }
+    fileReader.readAsText(file);
+  }
+  getNearestCustomer() {
+    const filteredData: any = [];
+    this.dataSource.forEach(element => {
+      if (element.dist >= 100) {
+        filteredData.push(element);
+      }
+    });
+    this.dataSource = _sortBy(filteredData, 'user_id');
+    this.table.renderRows();
+  }
 }
